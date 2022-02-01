93e2ac3c23a1d3490fcf416654979c09de1e59b1
caio <caioguimaraes12@outlook.com>
2022-02-01 01:31:24 -0300

    projeto raiz

---
+++ android/app/src/main/kotlin/br/com/academiadoflutter/vakinha_burguer_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.academiadoflutter.vakinha_burguer_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:vakinha_burguer_mobile/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
